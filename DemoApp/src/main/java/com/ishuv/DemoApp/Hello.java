675a2117580ae69a7a0e9b2a1a8440318f137018
Mr Dan <danwiz013@gmail.com>
2025-02-13 16:27:08 -0500

    first commit

---
+++ DemoApp/src/main/java/com/ishuv/DemoApp/Hello.java
@@ -0,0 +1,13 @@
+package com.ishuv.DemoApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/")
+    public String greet(){
+        return "Hello World, welcome to Ishuv";
+    }
+}
